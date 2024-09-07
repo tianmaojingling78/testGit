353f1ab2670b9b733db3bc73d48f27375c847bab
zhaoduo <zhao13655035@163.com>
2024-09-08 03:23:16 +0800

    before cherry-pick

--- TestRemote.java
+++ TestRemote.java
@@ -2,5 +2,7 @@ public class TestRemote {
     public static void main(String[] args) {
         System.out.println("第一次提交");
         System.out.println("new branch");
+
+        System.out.println("未提交修改，但是要切换分支");
     }
 }
---
+++ newTest.java
@@ -0,0 +1,5 @@
+public class newTest {
+    public static void main(String[] args) {
+        TestRemote remote = new TestRemote();
+    }
+}
