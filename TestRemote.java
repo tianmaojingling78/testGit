dc19b890719d0c1ca7064e3574c238f7f20859c3
zhaoduo <zhao13655035@163.com>
2024-09-08 03:30:08 +0800

    分支比较

--- TestRemote.java
+++ TestRemote.java
@@ -1,9 +1,10 @@
 public class TestRemote {
     public static void main(String[] args) {
         System.out.println("第一次提交");
+        System.out.println("修改分支main");
         System.out.println("new branch");
         System.out.println("增加一行");
 
-        System.out.println("未提交修改，但是要切换分支");
+        System.out.println("push后，增加一行");
     }
 }
